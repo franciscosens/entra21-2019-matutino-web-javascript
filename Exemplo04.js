e43e7b60cbec4d87a1b66936a24ee1d3c7271a7a
Francisco Lucas Sens <francisco.lucas.sens@gmail.com>
2019-08-02 08:10:32 -0300

    Desenvolvido a funcionalidade de validação

--- Exemplo04.js
+++ Exemplo04.js
@@ -1,8 +1,8 @@
 tarefas = [];
 
-function salvar(e){
+function salvar(e) {
     // 13 é o código do enter no teclado
-    if(e.keyCode == 13){
+    if (e.keyCode == 13) {
         adicionar();
     }
 }
@@ -11,7 +11,7 @@ function adicionar() {
     var campoNome = document.getElementById("nome");
     var nome = campoNome.value;
     valido = validar(nome, campoNome);
-    if(valido == false){
+    if (valido == false) {
         // mostrar feedback
         return;
     }
@@ -24,7 +24,7 @@ function adicionar() {
     var elementoBotaoEditar = document.createElement("button");
     elementoBotaoEditar.innerHTML = "Editar";
     elementoBotaoEditar.classList.add("btn", "btn-primary", "mr-2");
-    
+
     var elementoBotaoApagar = document.createElement("button");
     elementoBotaoApagar.innerHTML = "Apagar";
     elementoBotaoApagar.classList.add("btn", "btn-danger");
@@ -52,11 +52,46 @@ function editar() { }
 function atualizarQuantidade() {
     document.getElementById("quantidade")
         .innerHTML = tarefas.length;
- }
+}
 
 function limparCampo(campo) {
     campo.value = "";
     campo.focus();
- }
+}
 
-function validar(nome, campo) { }
+function validar(nome, campo) {
+    texto = '';
+    if (nome.trim().length == 0) {
+        texto = 'Nome deve ser preenchido';
+    } else if (nome.trim().length < 3) {
+        texto = 'Nome deve conter no mínimo 3 caracteres';
+    } else if (nome.trim().length > 20) {
+        texto = 'Nome deve conter no máximo 20 caracteres';
+    }
+
+    campo.classList.remove('border-danger', 'text-danger');
+
+    var elementos = document.getElementsByClassName('span-erro');
+
+    for (var i = 0; i < elementos.length; i++) {
+        var elemento = elementos[i];
+        var elementoPai = elemento.parentNode;
+        elementoPai.removeChild(elemento);
+    }
+
+    if (texto != '') {
+        campo.classList.add('border-danger', 'text-danger');
+
+        var spanErro = document.createElement('span');
+        spanErro.innerHTML = texto;
+        spanErro.classList.add('span-erro','text-danger', 'font-weight-bold');
+
+        var elementoPaiDoInput = campo.parentNode;
+        elementoPaiDoInput.appendChild(spanErro);
+
+        campo.focus();
+        return false;
+    }
+
+    return true;
+}
